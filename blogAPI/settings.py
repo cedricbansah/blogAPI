d6424f5076d3416a977f5ba3b0afe3d203277738
Cedric Bansah <cedibans@gmail.com>
2023-07-01 16:55:36 +0000

    final deployment

--- blogAPI/settings.py
+++ blogAPI/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'blog',
     'rest_framework_swagger',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- blogAPI/urls.py
+++ blogAPI/urls.py
@@ -14,16 +14,46 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import permissions
+#from Demos.FileSecurityTest import permissions
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls.conf import include
 from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-schema_view = get_swagger_view(title="Cedric's Blog API")
+#schema_view = get_swagger_view(title="Cedric's Blog API")
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cedric's Blog API",
+        default_version="v1",
+        description="",
+        terms_of_service="",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include("blog.urls")),
-    path('', schema_view)
+    #path('', schema_view),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
